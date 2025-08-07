d1493fc90c57a443ea2fe7a44f1f25fffbf38fef
Ivana <ishmedtje@gmail.com>
2025-08-07 17:51:21 -0300

    evitar que amigos se repitan

--- app.js
+++ app.js
@@ -2,26 +2,32 @@
 const amigos = [];
 
 function agregarAmigo() {
-    // 1. Capturar el valor del input
+    //  Capturar el valor del input
     const input = document.getElementById("amigo");
     const nombre = input.value.trim(); // trim() elimina espacios al principio y al final
 
-    // 2. Validar que no esté vacío
+    // Validar que no esté vacío
     if (nombre === "") {
         alert("Por favor, inserte un nombre.");
         return;
     }
 
-    // 3. Agregar el nombre al array
+    //hacer que el nombre no se repita  
+    if (amigos.includes(nombre)) {
+        alert("Ese nombre ya fue agregado.");
+        return;
+    }
+
+    //  Agregar el nombre al array
     amigos.push(nombre);
 
-    // 4. Mostrar el nombre en la lista de amigos
+    //  Mostrar el nombre en la lista de amigos
     const lista = document.getElementById("listaAmigos");
     const item = document.createElement("li");
     item.textContent = nombre;
     lista.appendChild(item);
 
-    // 5. Limpiar el campo de entrada
+    //  Limpiar el campo de entrada
     input.value = "";
 
 
